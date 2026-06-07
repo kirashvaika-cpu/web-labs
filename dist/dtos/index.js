7206758c01186eddd9d01c959839ce5205a298df
Kira <kira.shvaika@gmail.com>
2026-06-07 15:01:08 +0300

    chore: додано dist/ в .gitignore

--- dist/dtos/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-// ========================
-// USER DTOs
-// ========================
-Object.defineProperty(exports, "__esModule", { value: true });
--- dist/models/models.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-// src/models/models.ts
-// Внутрішні моделі даних — повторюють структуру БД
-Object.defineProperty(exports, "__esModule", { value: true });
